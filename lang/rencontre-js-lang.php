659233ab02ab0d71f965f95cb4a6f5dbb98bcc6e
sojahu <sojahu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-24 17:13:26 +0000

    update 2 - ES language
    git-svn-id: https://plugins.svn.wordpress.org/rencontre/trunk@954279 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lang/rencontre-js-lang.php
+++ lang/rencontre-js-lang.php
@@ -1,16 +1,16 @@
 <?php
 $lang = array(
-	'supp_photo'=>__('Supprimer cette photo ','rencontre'),
+	'supp_photo'=>__('Supprimer cette photo','rencontre'),
 	'supp_la_photo'=>__('Supprimer la photo','rencontre'),
 	'ajouter_photo'=>__('Ajouter la photo ','rencontre'),
-	'champs_incomplets'=>__(' champs incomplets ou erreur','rencontre'),
+	'champs_incomplets'=>__('champs incomplets ou erreur','rencontre'),
 	'mauvais_pseudo'=>__('Pseudo non disponible','rencontre'),
 	'conf_supp_compte'=>__('Confirmer la suppression du compte ?','rencontre'),
 	'nouv_pass_diff'=>__('Les nouveaux mots de passes sont diff&eacute;rents','rencontre'),
 	'pass_init_faux'=>__('Le mot de passe original est faux','rencontre'),
 	'ecrire_appuyer'=>__('Ecrire et appuyer <Enter>','rencontre'),
 	'tchat_attendre'=>__('Demande de Tchat envoy&eacute;e. Attendre...','rencontre'),
-	'demande_tchat'=>__('Vous avez une demande de Tchat : ','rencontre'),
+	'demande_tchat'=>__('Vous avez une demande de Tchat','rencontre'),
 	'ignorer'=>__('Ignorer','rencontre'),
 	'accepter'=>__('Accepter','rencontre'),
 	'ferme_fenetre'=>__('Votre interlocuteur &agrave; ferm&eacute; sa fen&ecirc;tre.','rencontre')
