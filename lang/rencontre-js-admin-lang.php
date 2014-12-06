f5803a7dfda180a15bf5900090867a283f4a2d01
sojahu <sojahu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-06 16:35:45 +0000

    Version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/rencontre/tags/1.4@1039463 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lang/rencontre-js-admin-lang.php
@@ -0,0 +1,24 @@
+<?php
+$lang = array(
+	'mod_valeur'=>__('Modifier la valeur','rencontre'),
+	'sauvegarde'=>__('Sauvegarde','rencontre'),
+	'confirmer'=>__('Confirmer','rencontre'),
+	'supp'=>__('supp','rencontre'),
+	'supprimer'=>__('Supprimer','rencontre'),
+	'categ'=>__('Cat&eacute;gorie','rencontre'),
+	'valeur'=>__('Valeur','rencontre'),
+	'nouvelit'=>__('Nouvel item','rencontre'),
+	'ajoute'=>__('Ajoute','rencontre'),
+	'pays'=>__('Pays','rencontre'),
+	'region'=>__('R&eacute;gion','rencontre'),
+	'code_iso'=>__('Code ISO','rencontre'),
+	'fichier_im'=>__('Fichier image','rencontre'),
+	'ajouter_photo'=>__('Ajouter la photo ','rencontre'),
+	'supp_la_photo'=>__('Supprimer la photo','rencontre'),
+	'supp_photo'=>__('Supprimer cette photo','rencontre'),
+	'supp_compte'=>__('Supprimer ce compte ?','rencontre'),
+	'libre_mail'=>__('Lib&eacute;rer ce mail ?','rencontre'),
+
+	);
+	// rencobjet.sauvegarde
+?>
---
+++ lang/rencontre-js-lang.php
@@ -0,0 +1,19 @@
+<?php
+$lang = array(
+	'supp_photo'=>__('Supprimer cette photo','rencontre'),
+	'supp_la_photo'=>__('Supprimer la photo','rencontre'),
+	'ajouter_photo'=>__('Ajouter la photo ','rencontre'),
+	'champs_incomplets'=>__('champs incomplets ou erreur','rencontre'),
+	'mauvais_pseudo'=>__('Pseudo non disponible','rencontre'),
+	'conf_supp_compte'=>__('Confirmer la suppression du compte ?','rencontre'),
+	'nouv_pass_diff'=>__('Les nouveaux mots de passes sont diff&eacute;rents','rencontre'),
+	'pass_init_faux'=>__('Le mot de passe original est faux','rencontre'),
+	'ecrire_appuyer'=>__('Ecrire et appuyer <Enter>','rencontre'),
+	'tchat_attendre'=>__('Demande de Tchat envoy&eacute;e. Attendre...','rencontre'),
+	'tchat_dem_ok'=>__('Demande de Tchat accept&eacute;e. Vous pouvez commencer.','rencontre'),
+	'demande_tchat'=>__('Vous avez une demande de Tchat','rencontre'),
+	'ignorer'=>__('Ignorer','rencontre'),
+	'accepter'=>__('Accepter','rencontre'),
+	'ferme_fenetre'=>__('Votre interlocuteur &agrave; ferm&eacute; sa fen&ecirc;tre.','rencontre')
+	);
+?>
